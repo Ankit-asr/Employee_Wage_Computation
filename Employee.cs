a496d39c604685ce17b43366a615c5451df8760c
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-07-25 11:38:23 +0530

    [Ankit] Add : Calculate Employee Daily Wage

--- Employee.cs
+++ Employee.cs
@@ -6,15 +6,31 @@ namespace Employee_Wage_Computation
 {
     class Employee
     {
+        //Constants
         const int IS_FULL_TIME = 1;
-        public void Employee_Attendance()
+        const int EMP_RATE_PER_HOUR = 20;
+        //Global Variables
+        int empHrs = 0;
+        int empWage = 0;
+        /// <summary>
+        /// Check daliy Wage of an Employee
+        /// </summary>
+        public void DailyEmployeeWage()
         {
             Random random = new Random();
             int result = random.Next(0, 2);
             if (result == IS_FULL_TIME)
+            {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
+            }
             else
+            {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Daily Wage of Employee is : " + empWage);
         }
     }
 }
