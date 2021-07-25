97345d26f3081d18a230eedbf33c7f41399644c9
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-07-25 10:44:39 +0530

    [Ankit] Add : Employee Wage Computation project

---
+++ Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_Wage_Computation
+{
+    class Employee
+    {
+        public void Welcome()
+        {
+            Console.WriteLine(" Welcome to the Employee Wage Computation ");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Employee_Wage_Computation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee obj = new Employee();
+            obj.Welcome();
+        }
+    }
+}
