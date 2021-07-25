04518764358a0bdbd32e189afe45581cd6eeea8b
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-07-25 11:05:52 +0530

    [Ankit] Add : Check Employee Present or Absent using Random function

--- Employee.cs
+++ Employee.cs
@@ -6,10 +6,15 @@ namespace Employee_Wage_Computation
 {
     class Employee
     {
-        public void Welcome()
+        const int IS_FULL_TIME = 1;
+        public void Employee_Attendance()
         {
-            Console.WriteLine(" Welcome to the Employee Wage Computation ");
-
+            Random random = new Random();
+            int result = random.Next(0, 2);
+            if (result == IS_FULL_TIME)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
         }
     }
 }
