d9248fcc2aa5aae807b16c02c8f435049c0c43f7
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-08-12 00:27:25 +0530

    [Ankit] Add: Total Wage For Each Company

---
+++ Employee_Wage_Computation/EmployeeWageBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_Wage_Computation
+{
+    class EmployeeWageBuilder
+    {
+        public string company;
+        public int empWagePerHour;
+        public int maxWorkingDaysInMonth;
+        public int maxHoursPerMonth;
+        public const int IS_FULL_TIME = 2;
+        public const int IS_PART_TIME = 1;
+        public int empHrs = 0;
+        public int empWage = 0;
+        public EmployeeWageBuilder(string company, int empWagePerHour, int maxWorkingDaysInMonth, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empWagePerHour = empWagePerHour;
+            this.maxWorkingDaysInMonth = maxWorkingDaysInMonth;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void EmployeeWage()
+        {
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < maxWorkingDaysInMonth)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * empWagePerHour;
+                totalEmpHrs += empHrs;
+            }
+            int totalEmpWage = totalEmpHrs * empWagePerHour;
+            Console.WriteLine(company + " Company Employee Wage for a Month is " + totalEmpWage);
+        }
+    }
+}
---
+++ Employee_Wage_Computation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Employee_Wage_Computation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeWageBuilder dmart = new EmployeeWageBuilder("DMART", 10, 20, 100);
+            EmployeeWageBuilder bridge = new EmployeeWageBuilder("BRIDGELABZ", 12, 25, 120);
+            dmart.EmployeeWage();
+            bridge.EmployeeWage();
+
+        }
+    }
+}
