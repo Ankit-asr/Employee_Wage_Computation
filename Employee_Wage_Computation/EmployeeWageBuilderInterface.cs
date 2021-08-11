8b4d0bbeb1fd6c7ab87ea1e952bf5f7b7ba79f63
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-08-12 01:08:12 +0530

    [Ankit] Refactor: code to implement Interface

---
+++ Employee_Wage_Computation/EmployeeWageBuilderInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_Wage_Computation
+{
+    interface EmployeeWageBuilderInterface
+    {
+        public void Wage_Computation();
+        public void Display(CompanyEmployeeWage cmp);
+    }
+}
